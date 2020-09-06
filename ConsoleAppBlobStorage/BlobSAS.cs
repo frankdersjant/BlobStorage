bb6f73363d54552104b7207ad7c7627c9227e9b1
frank <f.dersjant@quicknet.nl>
2020-09-06 12:33:59 +0200

    Add project files.

---
+++ ConsoleAppBlobStorage/BlobSAS.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleAppBlobStorage.Settings;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+
+namespace ConsoleAppBlobStorage
+{
+    public static class BlobSAS
+    {
+        private static readonly string policyName = "testpolicy";
+
+        public static void SassingTheBLOB()
+        {
+            CloudStorageAccount storageAccount = Common.CreateStorageAccountFromConnectionString();
+
+            // Create a blob client for interacting with the blob service.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // get a reference to the container
+            var blobContainer = blobClient.GetContainerReference("sampleblob");
+            blobContainer.CreateIfNotExists();
+
+            // create the stored policy we will use, with the relevant permissions and expiry time
+            var storedPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(10),
+                Permissions = SharedAccessBlobPermissions.Read |
+                              SharedAccessBlobPermissions.Write |
+                              SharedAccessBlobPermissions.List
+            };
+
+            // get the existing permissions (alternatively create new BlobContainerPermissions())
+            var permissions = blobContainer.GetPermissions();
+
+            // optionally clear out any existing policies on this container
+            permissions.SharedAccessPolicies.Clear();
+            // add in the new one
+            permissions.SharedAccessPolicies.Add(policyName, storedPolicy);
+            // save back to the container
+            blobContainer.SetPermissions(permissions);
+
+            // Now we are ready to create a shared access signature based on the stored access policy
+            var containerSignature = blobContainer.GetSharedAccessSignature(null, policyName);
+            // create the URI a client can use to get access to just this container
+            var uri = blobContainer.Uri + containerSignature;
+        }
+    }
+}
---
+++ ConsoleAppBlobStorage/IBlobStorage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Storage.Blob;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBlobStorage
+{
+    public interface IBlobStorage
+    {
+        Task<CloudBlockBlob> UploadBlobAsync(byte[] blobByteArray, string blobname, string title, string description);
+        Task<bool> CheckIfBlobExistsAsync(string blobName);
+        Task<IEnumerable<CloudBlockBlob>> ListBlobsAsync(string prefix = null, bool includeSnapshots = false);
+        Task DownloadBlobAsync(CloudBlockBlob cloudBlockBlob, Stream targetStream);
+        Task OverwriteBlobAsync(CloudBlockBlob cloudBlockBlob, byte[] blobByteArray, string name);
+        Task DeleteBlobAsync(CloudBlockBlob cloudBlockBlob, string blobname);
+
+        //Tip: Tuple<T>
+        Task UpdateMetadataAsync(CloudBlockBlob cloudBlockBlob, string title, string description, string name);
+       
+        string GetBlobUriWithSasToken(CloudBlockBlob cloudBlockBlob);
+
+        //Optional
+        Task<string> AcquireOneMinuteLeaseAsync(CloudBlockBlob cloudBlockBlob);
+        Task RenewLeaseAsync(CloudBlockBlob cloudBlockBlob, string leaseId);
+        Task ReleaseLeaseAsync(CloudBlockBlob cloudBlockBlob, string leaseId);
+       
+    }
+}
